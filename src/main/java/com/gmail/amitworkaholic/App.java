6cd155999aa74aadfc5d91606ac58e9eae83b88f
Amit Thakur <amitworkaholic@gmail.com>
2021-10-06 19:15:33 +0530

    Initial Commit

---
+++ src/main/java/com/gmail/amitworkaholic/App.java
@@ -0,0 +1,10 @@
+package com.gmail.amitworkaholic;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
